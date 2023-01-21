ba3c9f24086dc9dca742a36d09ab4924a8392adc
KilianKemgne <108962886+KilianKemgne@users.noreply.github.com>
2023-01-22 00:30:38 +0100

    FrontEnd Starter code

--- ChatApp_FrontEnd/src/environments/environment.prod.ts
+++ ChatApp_FrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'mongodb+srv://iotproject:iRiFt5vwOv97VPx0@iotproject.jkezxna.mongodb.net/?retryWrites=true&w=majority'
-};
+  apiBaseUrl: ''
+}
