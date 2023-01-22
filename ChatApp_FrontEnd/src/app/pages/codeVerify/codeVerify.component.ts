aa7ae2075326e647e0ea2e6a80c18c05c4cc20cf
KilianKemgne <108962886+KilianKemgne@users.noreply.github.com>
2023-01-22 08:55:16 +0100

    update

---
+++ ChatApp_FrontEnd/src/app/pages/codeVerify/codeVerify.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './codeVerify.component.html',
+  styleUrls: ['./codeVerify.component.scss']
+})
+export class CodeVerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
