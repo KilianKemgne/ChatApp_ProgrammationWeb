f797b32af17538ca7f60d7b3f216c40628e62008
menraromial <menraromial@gmail.com>
2023-01-22 20:38:25 +0100

    Update EnvoiSMS.js

--- ChatApp_BackEnd/endpoints/EnvoiSMS.js
+++ ChatApp_BackEnd/endpoints/EnvoiSMS.js
@@ -1,10 +1,27 @@
 
-const express = require('express')
-const router = express.Router()
+const API_TOKEN = 'YOUR_API_token';
 
-router.get('/', (req, res, next)=>{
-    // pour les sms
-    res.send('Envoie de SMS !')
-})
 
-module.exports = router
+import fetch from 'node-fetch';
+
+module.exports.sendSms = async (tel, message) => {
+  const resp = await fetch(
+    'https://asap-desk.com/api/v0/sms/message',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + API_TOKEN
+      },
+      body: JSON.stringify({
+        "phoneNumber": tel,
+        "message": message,
+        "externalId": "",
+        "lineId": ""
+     })
+    }
+  );
+
+  const data = await resp.json();
+  console.log(data);
+}
