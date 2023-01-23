fa1868997427d00b629e742e23c432c5cfb6ac59
Steve Lening <micheltalaupa@gmail.com>
2023-01-23 14:03:26 +0100

    Mise a jour du projet

--- ChatApp_BackEnd/controllers/user.controller.js
+++ ChatApp_BackEnd/controllers/user.controller.js
@@ -91,3 +91,52 @@ sequelize.sync().then(()=>{
 }).catch((error)=>{
     console.error('Impossible de creer cette table')
 })
+
+let CreateUser = (username, password, phonenumber, emailaddress) =>{
+    sequelize.sync().then(()=>{
+        User.create({
+            username: username,
+            password: password,
+            phonenumber: phonenumber,
+            // emailaddress: emailaddress
+        }).then((res)=>{
+            console.log(res)
+        }).catch((error)=>{
+            console.error("Echec de creation de l'utilisateur", error)
+        })
+     
+    }).catch((error)=>{
+        console.error('Impossible de creer cette table')
+    })
+}
+
+let FindAllUsers = ()=>{
+    sequelize.sync().then(()=>{
+        User.findAll().then((res)=>{
+            console.log(res)
+        }).catch((error)=>{
+            console.error("Echec de recherche des utilisateurs", error)
+        })
+    
+    }).catch((error)=>{
+        console.error('Impossible de creer cette table')
+    })
+}
+
+let FindUserByID = () =>{
+    
+}
+
+let FindUserByUserName = () =>{
+    
+}
+
+let UpdateUser = () =>{
+
+}
+
+let DeleteUser = () =>{
+
+}
+
+module.exports = {CreateUser, FindAllUsers, FindUserByID, FindUserByUserName, UpdateUser, DeleteUser}
--- ChatApp_BackEnd/endpoints/LogOut.js
+++ ChatApp_BackEnd/endpoints/LogOut.js
@@ -1,10 +1,14 @@
 
 const express = require('express')
 const router = express.Router()
+const userCRUD = require('../controllers/user.controller')
 
 router.get('/', (req, res, next)=>{
     // on detruit la session et on va a l'acceuil (/public)
     res.send('Deconnexion !')
 })
+
+// petit test de la methode findallusers
+userCRUD.FindAllUsers()
 
 module.exports = router
