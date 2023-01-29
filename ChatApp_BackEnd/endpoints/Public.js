280b6a66ffcb9ebc5743b93b460b7478a6cd3052
Sherelle20 <61359991+Sherelle20@users.noreply.github.com>
2023-01-29 12:10:07 +0100

    Update Public.js

--- ChatApp_BackEnd/endpoints/Public.js
+++ ChatApp_BackEnd/endpoints/Public.js
@@ -3,7 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next)=>{
-    // des qu'on lance l'application, on aboutit ici
     res.send('Connection etablie, bienvenue !')
     console.log('vous etiez deja connectes')
     console.log(req.session) 
