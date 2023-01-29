689b198f96f26c1f539968c9a800709dbccab049
Sherelle20 <61359991+Sherelle20@users.noreply.github.com>
2023-01-29 12:13:30 +0100

    Update SauvegardeRecuperationContacts.js

--- ChatApp_BackEnd/endpoints/SauvegardeRecuperationContacts.js
+++ ChatApp_BackEnd/endpoints/SauvegardeRecuperationContacts.js
@@ -4,19 +4,14 @@ const router = express.Router()
 const contactController = require('../controllers/contact.controller')
 
 
-// Route pour récupérer tous les contacts
 router.get('/', contactController.findAll);
 
-// Route pour créer un nouveau contact
 router.post('/', contactController.create);
 
-// Route pour récupérer un contact par ID
 router.get('/:id', contactController.findOne);
 
-// Route pour mettre à jour un contact par ID
 router.put('/:id', contactController.update);
 
-// Route pour supprimer un contact par ID
 router.delete('/:id', contactController.delete);
 router.post('/import',contactController.importContact)
 
