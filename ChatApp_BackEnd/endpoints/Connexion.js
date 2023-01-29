2c39306ab650cb48b87b2b8b537e32c56d14233b
Sherelle20 <61359991+Sherelle20@users.noreply.github.com>
2023-01-29 12:05:52 +0100

    Update Connexion.js

--- ChatApp_BackEnd/endpoints/Connexion.js
+++ ChatApp_BackEnd/endpoints/Connexion.js
@@ -8,7 +8,6 @@ const UserModel = require('../modele/user.model')
 const User = UserModel.User
 const sequelize = UserModel.sequelize
 
-// // on teste la connexion a la base de donnees
 sequelize.authenticate()
     .then(()=>{
         console.log('Connection a la BD reussie')
@@ -52,20 +51,19 @@ async function checkUser(username, password) {
 }
 
 router.post('/', (req, res, next)=>{
-    // on recupere le corps de la requete post
+    
     username = req.body.username
     password = req.body.password
     console.log('\n', req.body)
  
-    // on verifie si cet utilisateur existe dans la BD (on hache le mot de passe)
+    
     checkUser(username, md5(password))
 
-    // on renvoie le resultat de la requete au client
+  
     setTimeout(()=>{
-        // on va juste patienter 50 millisecondes pour que le resultat de la requete soit disponible
         if(id == undefined || phonenumber == undefined){
-            //on retourne une erreur
-            res.send({})// on renvoi une erreur
+            
+            res.send({})
             console.log('utilisateur inexistant')
         }
         else{
@@ -75,9 +73,7 @@ router.post('/', (req, res, next)=>{
                     emailaddress: emailaddress,
                     username: username
                 }
-                // req.session.sessionid = id.toString(10)+phonenumber.toString(10);
-                // req.session.userid = id
-                // req.session.username = username
+               
                 console.log(`Vous etes connectes, bienvenue ${req.session.user.username} !`)
                 console.log(req.session)
             }
@@ -85,7 +81,6 @@ router.post('/', (req, res, next)=>{
                 console.log('vous etiez deja connectes')
                 console.log(req.session) 
             }
-            //on renvoi le resultat (on ne lui envoi pas le password)
             res.send({'id': id, 'username': username, 'phonenumber': phonenumber,'emailaddress': emailaddress})
         }
     }, 100)
