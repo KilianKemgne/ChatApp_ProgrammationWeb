97520d58eeab5052c93531f5fa99d7eec24e5e1a
Sherelle20 <61359991+Sherelle20@users.noreply.github.com>
2023-01-29 12:07:30 +0100

    Update LogOut.js

--- ChatApp_BackEnd/endpoints/LogOut.js
+++ ChatApp_BackEnd/endpoints/LogOut.js
@@ -5,7 +5,6 @@ const userCRUD = require('../controllers/user.controller')
 const md5 = require("md5")
 
 router.get('/', async (req, res, next)=>{
-    // on detruit la session et on va a l'acceuil (/public)
     await req.session.destroy()
     console.log('Deconnexion reussi')
     console.log('req.session:', req.session)
