908d027e6cef6bb2552ec4bad2d184c55c4e7bcc
Sherelle20 <61359991+Sherelle20@users.noreply.github.com>
2023-01-29 12:21:39 +0100

    Update UpdateuserPassword.js

--- ChatApp_BackEnd/endpoints/UpdateuserPassword.js
+++ ChatApp_BackEnd/endpoints/UpdateuserPassword.js
@@ -32,11 +32,11 @@ async function checkUser(id) {
             if(res != null){
                 emailaddress = res.dataValues.emailaddress
                 password = res.dataValues.password
-                // on met a jour ses donnees dans la table user
+              
                 
                 password = newpassword
                 
-                // mise a jour de la BD
+               
                 User.update(
                     {
                         password: md5(password),
@@ -59,7 +59,7 @@ async function checkUser(id) {
 }
 
 router.post('/', (req, res, next)=>{
-    // on recupere le corps de la requete post
+   
     id = req.body.id
     newusername = req.body.userName
     newemailaddress = req.body.emailAddress
@@ -69,20 +69,20 @@ router.post('/', (req, res, next)=>{
     console.log('\n', req.body)
     console.log('req.session:', req.session)
  
-    // on verifie si cet utilisateur existe dans la BD (on hache le mot de passe)
+    
     checkUser(id)
 
-    // on renvoie le resultat de la requete au client
+  
     setTimeout(()=>{
-        // on va juste patienter 50 millisecondes pour que le resultat de la requete soit disponible
+        
         console.log('req.session:', req.session)
         if(password == undefined ){
-            //on retourne une erreur
-            res.send({})// on renvoi une erreur
+           
+            res.send({})
             console.log('utilisateur inexistant')
         }
         else{
-            // on met a jour la session
+            
             req.session.user = {
                 id: id,
                 emailaddress: emailaddress,
@@ -91,7 +91,7 @@ router.post('/', (req, res, next)=>{
             console.log(`La session a ete mise a jour, bienvenue ${req.session.user.username} !`)
             console.log(req.session)
 
-            //on renvoi le resultat (on ne lui envoi pas le password)
+           
 
             res.send({'id': id, 'username': username, 'phonenumber': phonenumber,'emailaddress': emailaddress})
         }
